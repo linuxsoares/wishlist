e6a69ce759c5131267ee9254bdf05b3317ced127
Gilmar Soares <linux.soares@gmail.com>
2020-04-01 02:06:42 -0300

    Add CI

--- test/wishlist/handler_test.clj
+++ test/wishlist/handler_test.clj
@@ -6,9 +6,4 @@
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 302)))))
